23af68e469d40aeff112742b947ec000e766f63e
wendel <wh68639@gmail.com>
2023-03-19 13:27:50 -0300

    procedimentos ok

--- main.js
+++ main.js
@@ -51,37 +51,72 @@ let pedicure1 = document.getElementById('pedicure1')
 let pedicure2 = document.getElementById('pedicure2')
 let pedicure3 = document.getElementById('pedicure3')
 
-let paragraph = document.querySelector('.teste')
+let manicure1 = document.getElementById('manicure1')
+let manicure2 = document.getElementById('manicure2')
+let manicure3 = document.getElementById('manicure3')
+
+
+
+let paragraphM = document.querySelector('.textosManiJs')
+let paragraphP = document.querySelector('.textosPediJs')
+
 
 pedicure1.addEventListener('click', procedimentos1)
 pedicure2.addEventListener('click', procedimentos2)
 pedicure3.addEventListener('click', procedimentos3)
 
-
+manicure1.addEventListener('click', procedimentos4)
+manicure2.addEventListener('click', procedimentos5)
+manicure3.addEventListener('click', procedimentos6)
 
 function procedimentos1() {
-    paragraph.style.transform="translateX(0)"
-    paragraph.style.transition="1.5s ease"
+    paragraphP.style.transform="translateX(0)"
+    paragraphP.style.transition="1.5s ease"
     pedicure1.style.color="#000"
     pedicure2.style.color="#9600AE"
     pedicure3.style.color="#9600AE"
 }
 
 function procedimentos2() {
-    paragraph.style.transform="translateX(-530px)"
-    paragraph.style.transition="1.5s ease"
+    paragraphP.style.transform="translateX(-530px)"
+    paragraphP.style.transition="1.5s ease"
     pedicure1.style.color="#9600AE"
     pedicure2.style.color="#000"
-    pedicure3.style.color="#9600AE"
+    pedicure3.style.color='#9600AE'
     
 }
 
 function procedimentos3() {
-    paragraph.style.transform="translateX(-1060px)"
-    paragraph.style.transition="1.5s ease"
+    paragraphP.style.transform="translateX(-1060px)"
+    paragraphP.style.transition="1.5s ease"
     pedicure1.style.color="#9600AE"
     pedicure2.style.color="#9600AE"
     pedicure3.style.color="#000"
+}
+
+function procedimentos4() {
+    paragraphM.style.transform="translateX(0)"
+    paragraphM.style.transition="1.5s ease"
+    manicure1.style.color="#000"
+    manicure2.style.color="#9600AE"
+    manicure3.style.color="#9600AE"
+}
+
+function procedimentos5() {
+    paragraphM.style.transform="translateX(-530px)"
+    paragraphM.style.transition="1.5s ease"
+    manicure1.style.color="#9600AE"
+    manicure2.style.color="#000"
+    manicure3.style.color="#9600AE"
+    
+}
+
+function procedimentos6() {
+    paragraphM.style.transform="translateX(-1060px)"
+    paragraphM.style.transition="1.5s ease"
+    manicure1.style.color="#9600AE"
+    manicure2.style.color="#9600AE"
+    manicure3.style.color="#000"
 }
 
 
@@ -147,4 +182,58 @@ sr.reveal(".lado-direito", {
     distance: "100px",
     duration: 1500,
     delay: 900
+})
+
+sr.reveal(".proced1", {
+    origin: "left",
+    distance: "100px",
+    duration: 3000,
+    delay: 500
+})
+
+sr.reveal(".proced2", {
+    origin: "right",
+    distance: "100px",
+    duration: 3000,
+    delay: 500
+
+})
+
+sr.reveal(".titleProcedimento", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000
+})
+
+sr.reveal("#imgProced1", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000
+})
+
+sr.reveal("#imgProced5", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000
+})
+
+sr.reveal("#imgProced2", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    delay: 500
+})
+
+sr.reveal("#imgProced4", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    delay: 500
+})
+
+sr.reveal("#imgProced3", {
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    delay: 1000
 })
